4459c09ae69bfe1ff011f41ced093678413a2132
J. Romero <JRomero.yyc@gmail.com>
2023-08-03 02:50:29 -0400

    Update after pull 02-08-2023
    Due to GCP platform trial ending, final pull was done without making alterations to the software. Find known issues in readme file

--- Restaurant_List_Gen_V1-0.py
+++ Restaurant_List_Gen_V1-0.py
@@ -1,37 +1,89 @@
-import googlemaps
-import pandas as pd
 import time
+import googlemaps # pip install googlemaps
+import pandas as pd # pip install pandas
+import re
 
-API_KEY = open('API_KEY.txt', 'r').read()
+## Google API Key is unique to all google API platform users,
+##    the key below belongs to the author of this script.
+API_KEY = 'KEY NEEDED'
 map_client = googlemaps.Client(API_KEY)
 
-## Rest_List_Gen(array[lat, long], radius, file)
-##      Generates a list of restaurants within the radius of location input
-##      The generated list is stored in file
-##      Output list is: (name, address, status, phone number, type, hours, )
-#Rest_List_Gen(
-location = (43.48011918142259, -80.53160353205661) # Waterloo
-radius = 10000 # 10km
+search_strings = ['bar', 'restaurant', 'pub', 'nightclub', 'tavern', 'bistro', 'cafe',
+                  'eatery', 'lounge', 'public house', 'drinkery', 'taphouse', 'grill', 
+                  'brewery', 'beer', 'alehouse', 'establishment'] ## list of search queries
+
+radius = 3 * 1000 # Radius in km
 business_list = []
-#)
-search_string = 'restaurant'
 
-#business_list.extend(response.get('results'))
-#next_page_token = response.get('next_page_token')
-next_page_token = 1
-
-while next_page_token:
-    time.sleep(2)
+for search_string in search_strings:
     response = map_client.places_nearby(
-        location = location,
-        keyword = search_string,
-        radius = radius,
-        page_token = next_page_token
+        location=(43.65162067487806, -79.47599887066528), ## Set to Runnymede station in Toronto
+        keyword=search_string,
+        radius=radius,
+        min_price=1,
+        max_price=4
     )
-    business_list.extend(response.get('results'))
-    next_page_token = response.get('next_page_token')
+
+    for place in response['results']:
+        place_details = map_client.place(
+            place_id=place['place_id'],
+            fields=['name', 
+                    'formatted_address', 
+                    'formatted_phone_number',
+                    'opening_hours',
+                    'business_status',
+                    'price_level',
+                    'reservable', 
+                    'delivery', 
+                    'dine_in', 
+                    'curbside_pickup', 
+                    'url',
+                    'website',
+                    'serves_beer', 
+                    'serves_wine',
+                    'rating', 
+                    'user_ratings_total',
+                    'reviews']
+        )
+        if place_details['result']['business_status'] == 'OPERATIONAL': ## No filtering by serves_beer
+            business_list.append(place_details['result'])
+        # if 'serves_beer' in place_details['result']: ## Filter by serves_beer and operational
+        #     if place_details['result']['serves_beer'] == True and place_details['result']['business_status'] == 'OPERATIONAL':
+        #         business_list.append(place_details['result'])
+
+## Extract top 3 reviews and put them into separate columns
+def extract_reviews(reviews):
+    if isinstance(reviews, float): # Addresses the float issue
+        return ['', '', '']
+    review_texts = []
+    for review in reviews[:3]:
+        review_texts.append(review['text'])
+    while len(review_texts) < 3:
+        review_texts.append('')
+    return review_texts
+
+## Extract weekday_text from opening hours and concatenate into one column
+def extract_weekday_text(opening_hours):
+    if not isinstance(opening_hours, dict) or 'weekday_text' not in opening_hours: # Addresses the float issue
+        return ''
+    return '|'.join(opening_hours['weekday_text'])
+
+## Extract postal code from the 'formatted_address' attribute
+def extract_postal_code(address):
+    for component in address.split(','):
+        component = component.strip()
+        match = re.search(r'\b[A-Z][0-9][A-Z] [0-9][A-Z][0-9]\b', component)
+        if match:
+            return match.group()
+    return ''                
 
 df = pd.DataFrame(business_list)
-df['url'] = 'www.google.com/maps/place/?q=place_id:' + df['place_id']
-
-df.to_excel('Restaurant list.xlsx', index=False)
+if df.empty:
+    print('No Search Results Error')
+else:
+    df = df.drop_duplicates(subset=['name', 'formatted_address'])  ## Remove duplicate rows based on name and address
+    df['postal_code'] = df['formatted_address'].apply(lambda x: extract_postal_code(x) if x else '')
+    df[['review_1', 'review_2', 'review_3']] = df['reviews'].apply(lambda x: pd.Series(extract_reviews(x)))
+    df['weekday_text'] = df['opening_hours'].apply(lambda x: extract_weekday_text(x) if x else '')
+    df = df.drop(columns=['reviews', 'opening_hours'])
+    df.to_excel('Runnymede_Station_3km.xlsx', index=False)
