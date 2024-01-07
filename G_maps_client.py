56cc21c9be935dcc952b17b98bfe3b7d26177807
J. Romero <JRomero.yyc@gmail.com>
2024-01-06 23:17:12 -0500

    Finalized

--- G_maps_client.py
+++
@@ -1,7 +0,0 @@
-import time
-import googlemaps # pip install googlemaps
-import pandas as pd # pip install pandas
-import re
-
-class GoogleMapsClient(self):
-    pass
--- README.md
+++
@@ -1,2 +0,0 @@
-# KWCC_Software_Project
- KW Craft Cider Inc. Prospect pulling and mapping software.
